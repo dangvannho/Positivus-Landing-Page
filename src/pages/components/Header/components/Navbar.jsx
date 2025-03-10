bc47754edf8630c03aa5a7bfa23fdec32f34ffd3
dangvannho <dangvannho7@gmail.com>
2025-03-10 14:40:21 +0700

    fix bug navbar

--- src/pages/components/Header/components/Navbar.jsx
+++ src/pages/components/Header/components/Navbar.jsx
@@ -1,22 +1,4 @@
-import { useEffect, useRef } from "react";
-
-const Navbar = ({ toggle, setToggle }) => {
-  const navRef = useRef(null);
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [setToggle]);
-
-  const handleClickOutside = (event) => {
-    if (navRef.current && !navRef.current.contains(event.target)) {
-      setToggle(false);
-    }
-  };
-
+const Navbar = ({ toggle }) => {
   return (
     <>
       <nav className="hidden lg:flex text-sm laptop:text-xl gap-10 ml-auto mr-10 ">
@@ -39,10 +21,7 @@ const Navbar = ({ toggle, setToggle }) => {
 
       {/* Mobile Navbar */}
       {toggle && (
-        <nav
-          ref={navRef}
-          className="absolute top-full w-full flex flex-col text-sm tablet:text-2xl text-center gap-5 bg-white py-5"
-        >
+        <nav className="absolute top-full w-full flex flex-col text-sm tablet:text-2xl text-center gap-5 bg-white py-5">
           <a href="#!" className="p-1 hover:bg-lime-green font-normal">
             About us
           </a>
